081f4594a6168a740e6a40cfc6e18dcf6bf6c60c
SabrinaMouginot <s.mouginot@yahoo.fr>
2023-02-06 16:34:20 +0100

    Apparition en js  de la taupe dans la grille qui change de case de maniere aleatoire

--- René.js
+++ René.js
@@ -2,3 +2,37 @@ const squares = document.querySelectorAll('.square')
 const mole = document.querySelector('.mole')
 const timeLeft = document.querySelector('#time-left')
 const score = document.querySelector('#score')
+
+let hitPosition
+
+function randomSquare() {
+    squares.forEach(square => {
+      square.classList.remove('mole')
+    })
+  
+    let randomSquare = squares[Math.floor(Math.random() * 9)]
+    randomSquare.classList.add('mole')
+  
+    hitPosition = randomSquare.id
+  }
+
+  // LA GRILLE
+
+squares.forEach(square => {
+    square.addEventListener('mouseup', () => {
+      if (square.id == hitPosition) {
+        result++
+        score.textContent = result
+        hitPosition = null
+      }
+    })
+  })
+
+  // MOUVEMENT DE LA TAUPE
+
+function moveMole() {
+    let timerId = null
+    timerId = setInterval(randomSquare, 500)
+  }
+  
+  moveMole()
